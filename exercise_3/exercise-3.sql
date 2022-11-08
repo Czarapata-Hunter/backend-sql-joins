9d8fb3e42b3ec8c5bfd95367b5e839b224bdb9d1
Hunter Czarapata <czarapata.hunter@gmail.com>
2022-11-08 11:57:43 -0800

    Tests up to 4 passing

--- exercise_3/exercise-3.sql
+++ exercise_3/exercise-3.sql
@@ -1,3 +1,15 @@
 -- find all film titles with the category Action
 -- use a LEFT JOIN FROM film onto film_category
 -- use LEFT JOIN FROM film_categor onto category
+SELECT 
+    film.title
+FROM 
+    film
+LEFT JOIN film_category ON film.film_id = film_category.film_id
+LEFT JOIN category ON category.category_id = film_category.category_id
+WHERE category.name = 'Action'
+
+-- SELECT column names
+-- FROM table1
+-- LEFT JOIN table2
+--   ON table1.common_column = table2.common_column;
